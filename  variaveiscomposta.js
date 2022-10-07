0b501f142de94b2835aa29d7cb2580191a1e2a08
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-10-06 22:47:05 -0300

    Update  variaveiscomposta.js

---  variaveiscomposta.js
+++  variaveiscomposta.js
@@ -1,4 +1,16 @@
-var num = [5,6,7,3,1]
-console.log(`O num vale ${num}`)
-console.log(`Tem ${num.length} números`)
-console.log(`E organizado fica ${num.sort}`)
+let num = [6,4,6,2,7]
+let cont = 0
+for (let conta = 0; conta<num.length; conta++)
+    console.log(`A posição ${conta} tem valor ${num[conta]}`)
+///OU PODEMOS SIMPLIFICAR
+console.log('---------------')
+for (let cont in num){
+    console.log(`A posição ${cont} vale ${num[cont]}`)
+}
+
+let posicao = num.indexOf(6)
+if (posicao == -1){
+    console.log('Infelizmente não achei o número 3')
+}else{
+    console.log(`Ele está na posição ${posicao}`)
+}
